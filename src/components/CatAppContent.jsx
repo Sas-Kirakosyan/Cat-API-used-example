59630dc7622fbf2acc68b55709db2165b0f966f5
Sasun Kirakosyan <sasun.kirakosyan@softconstruct.com>
2025-01-23 15:11:43 +0400

    add api key

--- src/components/CatAppContent.jsx
+++ src/components/CatAppContent.jsx
@@ -1,25 +1,36 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "./Card";
-
+const API_KEY =
+  "live_xSJse1L779LQBlpNBwKyUwjWzp7DZSVt9HrTT4wb7GcYsO9ieOFB72mmwo1qUWJP";
+// const page = 1;
+// const limit = 5;
 export default function CatAppContent() {
   const { id } = useParams();
 
   const [images, setImages] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchCats = async () => {
+      const headers = new Headers({
+        "Content-Type": "application/json",
+        "x-api-key": API_KEY,
+      });
+
+      var requestOptions = {
+        method: "GET",
+        headers: headers,
+        redirect: "follow",
+      };
+      setLoading(true);
       try {
+        setLoading(true);
         const response = await fetch(
-          `https://api.thecatapi.com/v1/images/search?limit=10&page=1&category_ids=${id}`
+          `https://api.thecatapi.com/v1/images/search?limit=5&order=ASC&page=1&category_ids=${id}`,
+          requestOptions
         );
-
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-
         const data = await response.json();
         setImages(data);
       } catch (err) {
