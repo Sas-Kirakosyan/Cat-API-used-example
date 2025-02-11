48c7c91dcb7363adaecc6580daccc9e13124e3bf
Sasun Kirakosyan <sasun.kirakosyan@softconstruct.com>
2025-02-11 16:37:49 +0400

    add console

--- src/pages/FishApp.jsx
+++ src/pages/FishApp.jsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const API_KEY = "0f74f183dd0f440dbed60406250702";
 
 const FishApp = () => {
+  const [state, setState] = useState(null);
+
   useEffect(() => {
     async function fetchData() {
       const fetchResult = await fetch(
-        `https://api.weatherapi.com/v1/marine.json?key=${API_KEY}&q=Sevan`
+        `https://api.weatherapi.com/v1/marine.json?key=${API_KEY}&q=Yerevan&days=5`
       );
       const result = await fetchResult.json();
-      console.log({ result });
+      setState(result);
+      console.log({ result }, state.forecast.forecastday[0]?.hour[6]);
     }
     fetchData();
   }, []);
 
-  return <div>fish app</div>;
+  console.log(state);
+  if (!state) return null;
+  return (
+    <div>fish app {state.forecast?.forecastday[0]?.hour[10]?.pressure_in} </div>
+  );
 };
 
 export default FishApp;
